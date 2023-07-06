05bff8f160d67cd1b28363dd5ae3461d5820ac84
oskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-07-07 01:29:28 +0700

    Menambahkan konsep layout

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -7,6 +7,6 @@ class Dashboard extends BaseController
 {
     public function index()
     {
-        return blade_view('dashboard', ['tes' => 'isi data']);
+        return blade_view('admin/dashboard', ['tes' => 'isi data']);
     }
 }
---
+++ app/Views/admin/dashboard.blade.php
@@ -0,0 +1,6 @@
+@extends('layout/navbar')
+
+@section('title', 'Halaman Dashboard')
+@section('mainContent')
+    <h1>Ini adalah isi dari konten</h1>
+@endsection
